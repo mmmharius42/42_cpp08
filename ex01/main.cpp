0a08370fff93b7dbd8bd8388ba5e036da29d2773
marius <mpapin@c1r4p7.42lehavre.fr>
2025-12-03 16:47:13 +0100

    ex02 finish

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -10,17 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Span.hpp"
+#include "includes/Span.hpp"
 
 int main()
 {
-    Span sp = Span(5);
-    sp.addNumber(6);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
+    try {
+        Span sp = Span(1);
+        sp.addNumber(17);
+        // sp.addNumber(9);
+        // sp.addNumber(11);
+        // sp.addNumbers(0, 51000);
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
+    }
+    catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
     return 0;
 }
